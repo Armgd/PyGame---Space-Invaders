ba623647e28093023ec70c27399fb448884166dd
Nicolas Gras <nicolas.gras@live.fr>
2019-11-21 19:20:16 +0100

    123

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ pygame.init()
 # Screen creation
 screen = pygame.display.set_mode((800, 600))
 
-while True:
-    pass
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('space-invaders.png')
+pygame.display.set_icon(icon)
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = Falses
+    screen.fill((0, 0, 0))
+    pygame.display.update()
